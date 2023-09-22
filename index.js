a79bd2fe7b2d008601010269e5404e43bacf5294
Flunder620 <jguzman12310@yahoo.com>
2023-09-21 23:48:53 -0500

    created questions array

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 // TODO: Include packages needed for this application
-
+const inquirer = require('inquirer');
+const fs = require('fs');
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        name: "title",
+        message: "What is your project title?"
+    },
+    {
+        type: "input",
+        name: "description",
+        message: "Enter your description: "
+    },
+    {
+        type: "input",
+        name: "install",
+        message: "Enter installation instructions: "
+    },
+    {
+        type: "input",
+        name: "usage",
+        message: "Enter usage information: "
+    },
+    {
+        type: "input",
+        name: "contribution",
+        message: "Enter your contribution guidelines: "
+    },
+    {
+        type: "input",
+        name: "test",
+        message: "Enter test instructions: "
+    },
 
+
+];
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+}
 
 // Function call to initialize app
 init();
