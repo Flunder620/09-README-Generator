4ba9a127ccaea3b1cf767c00f352e4dcd724a56b
Flunder620 <jguzman12310@yahoo.com>
2023-09-22 00:15:17 -0500

    imported function

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require ("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
 const questions = [
     {
@@ -58,6 +59,7 @@ function writeToFile(fileName, data) {}
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions)
+    generateMarkdown()
 }
 
 // Function call to initialize app
